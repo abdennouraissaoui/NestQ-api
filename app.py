968ab6b7b8d4ff748cc5c5692c378438577a8b90
Abdennour Aissaoui <abdennouraissaoui@gmail.com>
2020-04-06 21:44:13 -0400

    fixed create_db issue

--- app.py
+++ app.py
@@ -23,15 +23,6 @@ app.config['JWT_BLACKLIST_ENABLED'] = True
 app.secret_key = 'icecream'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    # create all the tables unless they exist
-    # when we import for instance Store, it goes to Store resource then Store Model and
-    # then sees the defintion of the db. If we don't import it won't create the table
-    db.create_all()
-
-
 jwt = JWTManager(app)
 
 
---
+++ run.py
@@ -0,0 +1,11 @@
+from app import app
+from db import db
+
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    # create all the tables unless they exist
+    # when we import for instance Store, it goes to Store resource then Store Model and
+    # then sees the defintion of the db. If we don't import it won't create the table
+    db.create_all()
