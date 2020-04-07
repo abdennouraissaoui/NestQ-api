4a31719d6cb5b4af910574840ecbc38d1c53668e
Abdennour Aissaoui <abdennouraissaoui@gmail.com>
2020-04-06 21:54:59 -0400

    added psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -18,7 +19,7 @@ from resources.analytics import Analytics
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.secret_key = 'icecream'
 api = Api(app)
