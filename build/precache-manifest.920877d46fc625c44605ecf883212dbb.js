06543dfd20a0cc289950ce375b6e26635e403ec8
Abdennour Aissaoui <abdennouraissaoui@gmail.com>
2020-04-10 21:38:19 -0400

    added build

---
+++ build/precache-manifest.920877d46fc625c44605ecf883212dbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53980a0ea011f276f068038aec9c2a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0979514b66984f41799",
+    "url": "/static/css/2.c941f3ff.chunk.css"
+  },
+  {
+    "revision": "1628194ccc103d8b7ac7",
+    "url": "/static/css/main.ebf85830.chunk.css"
+  },
+  {
+    "revision": "c0979514b66984f41799",
+    "url": "/static/js/2.c670cd6d.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.c670cd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1628194ccc103d8b7ac7",
+    "url": "/static/js/main.de88ed2c.chunk.js"
+  },
+  {
+    "revision": "ab1a97bc663f40638555",
+    "url": "/static/js/runtime-main.7f7a811b.js"
+  },
+  {
+    "revision": "707e91dce0deff4a5aa326006890d026",
+    "url": "/static/media/nestegg.707e91dc.png"
+  }
+]);
